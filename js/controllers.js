801a0f2d12a09c130e7e8b1fbbcef0febdff7430
JoseMontesRicardo <JoseMontesRicardo>
2016-09-02 14:52:59 -0500

    [add avatar]

--- js/controllers.js
+++ js/controllers.js
@@ -1,9 +1,42 @@
 angular.module('portfolioApp.controllers', [])
 
+	.controller('indexController', function($scope, $window,$timeout){
+
+		angular.element($('.web-header__content__nav__options__list__items'))
+			.bind('click', function(){
+				angular.element($('.web-header__content__nav__options__list__items>a')).removeClass('activate-no-hover')
+				angular.element($(this).children()).addClass('activate-no-hover')
+			})
+
+		angular.element($window)
+			.bind('scroll', function(e){
+				angular.element($('.header-body__title')).removeClass('_show')
+				$scope.scroll = $window.pageYOffset
+
+				if ( $scope.scroll > $scope.lastScroll ){//si hace scroll hacia abajo
+					if ( $window.scrollY > 210 )
+						angular.element(document.querySelector('.web-header')).css('boxShadow', '0 6px 10px #b6b6b6')
+					if ( $window.scrollY >= 30 )
+						angular.element($('.header-body__title')).addClass('_hide')
+						$timeout(function() {
+							angular.element($('.header-body__title')).removeClass('_hide')
+							angular.element($('.header-body__title')).hide()
+						}, 100)
+				} else {//si hace scroll hacia arriba
+					if ( $window.scrollY === 0 ){
+						angular.element(document.querySelector('.header-body')).css('height', '40vh')
+							angular.element($('.header-body__title')).show()
+						angular.element($('.header-body__title')).addClass('_show')
+						$timeout(function() {
+							angular.element($('.header-body__title')).removeClass('_show')
+						}, 100)
+					}
+					if ( $window.scrollY <= 210 )
+						angular.element(document.querySelector('.web-header')).css('boxShadow', 'none')
+				}
+				$scope.lastScroll = $scope.scroll
+		})
+	})
+
 	.controller('homeController', function($scope, $window){
-		// angular.element($window)
-		//  .bind('scroll', function(){
-		//  	alert('hola')
-		//  	// scope.$apply()
-		//  })
 	})
--- js/directives.js
+++ js/directives.js
@@ -4,27 +4,18 @@ angular.module('portfolioApp.directives', [])
 		return {
 			restrict: 'E',
 			templateUrl: 'templates/header-scroll.html',
-			controller: function($scope, $window){
-				// $scope.page='Home'
-				if (document.readyState === "complete") {
-					if ($window.scrollY !== 0 ){
-				 		angular.element(document.querySelector('.web-header')).css('boxShadow', '0 6px 10px #b6b6b6')
-				 	}	
+			controller: function($scope, $window , $element){
+				var headerBar = $element[0].childNodes[0]
+				
+				$scope.start = function(pageTitle){
+					$scope.pageTitle = pageTitle
 				}
-				angular.element($window)
-				 .bind('scroll', function(e){
-				 	console.log($window.scrollY)
-				 	if ( $window.scrollY === 0 ){
-				 		angular.element(document.querySelector('.header-body')).css('height', '35vh')
-				 	} else if ( $window.scrollY > 159 ){
-				 		angular.element(document.querySelector('.web-header')).css('boxShadow', '0 6px 10px #b6b6b6')
-				 	} else if ( $window.scrollY >= 10 ){
-				 		angular.element(document.querySelector('.web-header')).css('boxShadow', 'none')
+
+				if (document.readyState === "complete") {
+					if ($window.scrollY > 159){
+				 		headerBar.css('boxShadow', '0 6px 10px #b6b6b6')
 				 	}
-
-
-				 	angular.element(document.querySelector('.header-body')).css('height', 35-$window.scrollY)
-				 })
+				}
 			}
 		}
 	})
